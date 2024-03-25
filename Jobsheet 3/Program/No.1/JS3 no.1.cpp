ee5db77277987cc35713865348516036ada37f8e
GilangMulya <144107986+GilangMulya@users.noreply.github.com>
2024-03-25 20:48:13 +0700

    JS3 no.1.cpp

--- Jobsheet 3/Program/No.1/JS3 no.1.cpp
+++ Jobsheet 3/Program/No.1/JS3 no.1.cpp
@@ -1,37 +1,37 @@
 //Created By M.Gilang Mulya Putra_23343073
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> //Library standar input-output
+#include <stdlib.h> //Library standar alokasi memori
 
 void fibonacci(int n) {
-    int *fib = (int *)malloc(n * sizeof(int));
-    if (fib == NULL) {
-        printf("Error: Unable to allocate memory.");
-        return;
+    int *fib = (int *)malloc(n * sizeof(int)); // Mengalokasikan memori untuk menyimpan deret Fibonacci
+    if (fib == NULL) { // Memeriksa apakah alokasi memori berhasil
+        printf("Error: Unable to allocate memory."); // Cetak pesan kesalahan jika alokasi memori gagal
+        return; // Keluar dari fungsi jika alokasi memori gagal
     }
 
-    *fib = 0;
-    *(fib + 1) = 1;
+    *fib = 0; // Menginisialisasi anggota pertama deret Fibonacci
+    *(fib + 1) = 1; // Menginisialisasi anggota kedua deret Fibonacci
 
-    printf("Fibonacci sequence up to %d terms:\n", n);
-    printf("%d, %d, ", *fib, *(fib + 1));
+    printf("Fibonacci sequence up to %d terms:\n", n); // Cetak pesan awal untuk deret Fibonacci
+    printf("%d, %d, ", *fib, *(fib + 1)); // Cetak dua anggota pertama deret Fibonacci
 
+    // Menghitung dan mencetak anggota-anggota deret Fibonacci selanjutnya
     for (int i = 2; i < n; i++) {
-        *(fib + i) = *(fib + i - 1) + *(fib + i - 2);
-        printf("%d", *(fib + i));
+        *(fib + i) = *(fib + i - 1) + *(fib + i - 2); // Menghitung anggota deret Fibonacci ke-i
+        printf("%d", *(fib + i)); // Cetak anggota deret Fibonacci ke-i
         if (i != n - 1)
-            printf(", ");
+            printf(", "); // Cetak koma jika bukan anggota terakhir dalam deret
     }
 
-    free(fib);
+    free(fib); // Membebaskan memori yang dialokasikan untuk deret Fibonacci
 }
 
 int main() {
     int n;
     printf("Masukkan nomor ketentuan Fibonaci: ");
-    scanf("%d", &n);
+    scanf("%d", &n); // Menerima input dari pengguna
     
-    fibonacci(n);
+    fibonacci(n); // Memanggil fungsi untuk menghasilkan deret Fibonacci hingga n anggota
 
     return 0;
 }
-
